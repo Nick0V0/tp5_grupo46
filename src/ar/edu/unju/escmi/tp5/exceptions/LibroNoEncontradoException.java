881729f18468798fc4cd833159d75fb9beb2f419
Sebasti348 <valeriosebastian26@gmail.com>
2024-10-04 00:51:25 -0300

    Finalizada la Clase Exception

---
+++ src/ar/edu/unju/escmi/tp5/exceptions/LibroNoEncontradoException.java
@@ -0,0 +1,11 @@
+package ar.edu.unju.escmi.tp5.exceptions;
+
+public class LibroNoEncontradoException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public LibroNoEncontradoException(String errorMessage) {
+		super(errorMessage);
+
+	}
+
+}
