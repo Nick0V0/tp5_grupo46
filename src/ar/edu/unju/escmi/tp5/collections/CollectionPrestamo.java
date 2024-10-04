785cea22fb1dc0633299d68787112d4525c54a6a
Sebasti348 <valeriosebastian26@gmail.com>
2024-10-04 00:43:14 -0300

    Finalizacion de la Clase Collection de Prestamo

--- src/ar/edu/unju/escmi/tp5/collections/CollectionPrestamo.java
+++ src/ar/edu/unju/escmi/tp5/collections/CollectionPrestamo.java
@@ -1,5 +1,25 @@
 package ar.edu.unju.escmi.tp5.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unju.escmi.tp5.dominio.Prestamo;
+import ar.edu.unju.escmi.tp5.exceptions.LibroNoDisponibleException;
+
 public class CollectionPrestamo {
+	static public List<Prestamo> prestamos = new ArrayList<Prestamo>();
+
+	public static void agregarPrestamo(Prestamo prestamo) {
+		prestamos.add(prestamo);
+	}
+
+	public static Prestamo buscarPrestamoLibro(int id) throws LibroNoDisponibleException {
+		for (Prestamo prestamo : prestamos) {
+			if (prestamo.getLibro().getId() == id ) {
+				return prestamo;
+			}
+		}
+		throw new LibroNoDisponibleException("El libro con codigo " + id + " no fue encontrado");
+	}
 
 }
