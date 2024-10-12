f680176507925718bf42abaf72c5cfd85c5b8f9a
Sebasti348 <valeriosebastian26@gmail.com>
2024-10-12 17:37:37 -0300

    Finalizaciond de la Clase Bibliotecario

--- src/ar/edu/unju/escmi/tp5/dominio/Bibliotecario.java
+++ src/ar/edu/unju/escmi/tp5/dominio/Bibliotecario.java
@@ -1,5 +1,34 @@
 package ar.edu.unju.escmi.tp5.dominio;
 
-public class Bibliotecario {
+public class Bibliotecario extends Usuario {
+	private int legajo;
+
+	public Bibliotecario() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Bibliotecario(int legajo) {
+		super();
+		this.legajo = legajo;
+	}
+
+	@Override
+	public void mostrarDatos() {
+
+		System.out.println("Id:" + this.id);
+		System.out.println("Nombre:" + this.nombre);
+		System.out.println("Apellido:" + this.apellido);
+		System.out.println("Email:" + this.email);
+		System.out.println("Legajo:" + this.legajo);
+
+	}
+
+	public int getLegajo() {
+		return legajo;
+	}
+
+	public void setLegajo(int legajo) {
+		this.legajo = legajo;
+	}
 
 }
