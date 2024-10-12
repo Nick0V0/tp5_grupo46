43e56f644b9989f3596b58cd139bc9f233761636
Sebasti348 <valeriosebastian26@gmail.com>
2024-10-12 17:34:28 -0300

    Finalizada la Clase Alumno

--- src/ar/edu/unju/escmi/tp5/dominio/Alumno.java
+++ src/ar/edu/unju/escmi/tp5/dominio/Alumno.java
@@ -1,5 +1,49 @@
 package ar.edu.unju.escmi.tp5.dominio;
 
-public class Alumno {
+public class Alumno extends Usuario{
+	private String curso;
+	private int numeroLibreta;
+	
+	public Alumno() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public Alumno(String curso, int numeroLibreta) {
+		super();
+		this.curso = curso;
+		this.numeroLibreta = numeroLibreta;
+	}
+	
+	@Override
+	public void mostrarDatos() {
+		// TODO Auto-generated method stub
+		System.out.println("Id:"+this.id);
+		System.out.println("Nombre"+this.nombre);
+		System.out.println("Apellido:"+this.apellido);
+		System.out.println("Email:"+this.email);
+		System.out.println("Curso:"+this.curso);
+		System.out.println("NumLibreta:"+this.numeroLibreta);
+		
+	}
+	
+	
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public int getNumeroLibreta() {
+		return numeroLibreta;
+	}
+
+	public void setNumeroLibreta(int numeroLibreta) {
+		this.numeroLibreta = numeroLibreta;
+	}
+	
+	
+	
 }
