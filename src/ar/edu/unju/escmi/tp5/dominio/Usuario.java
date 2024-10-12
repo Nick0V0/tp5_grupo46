99a11aed9429fcc85a0759999108bf1355fb8412
Sebasti348 <valeriosebastian26@gmail.com>
2024-10-12 17:22:54 -0300

    Finalizacion de la Clase Usuario

--- src/ar/edu/unju/escmi/tp5/dominio/Usuario.java
+++ src/ar/edu/unju/escmi/tp5/dominio/Usuario.java
@@ -1,5 +1,43 @@
 package ar.edu.unju.escmi.tp5.dominio;
 
 public abstract class Usuario {
-	
+	protected int id;
+	protected String nombre;
+	protected String apellido;
+	protected String email;
+
+	public abstract void mostrarDatos();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 }
